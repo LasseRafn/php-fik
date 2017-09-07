c9e60bb9dfcb254933445fd39c79eab7032072aa
Lasse Rafn <lasserafn@gmail.com>
2017-09-07 14:04:18 +0000

    Apply fixes from StyleCI

--- src/FIK.php
+++ src/FIK.php
@@ -4,60 +4,60 @@ namespace LasseRafn\FIK;
 
 class FIK
 {
-	const REQUIRED_LENGTH = 15;
-	const INVOICE_ID      = 0;
-	const REMINDER_ID     = 3;
+    const REQUIRED_LENGTH = 15;
+    const INVOICE_ID = 0;
+    const REMINDER_ID = 3;
 
-	/**
-	 * Generate a FIK.
-	 *
-	 * @param int  $invoiceNumber
-	 * @param bool $isReminder
-	 *
-	 * @return string
-	 */
-	public static function generate( $invoiceNumber = 1, $isReminder = false )
-	{
-		$fik = str_pad( $invoiceNumber, 13, 0, STR_PAD_LEFT );
+    /**
+     * Generate a FIK.
+     *
+     * @param int  $invoiceNumber
+     * @param bool $isReminder
+     *
+     * @return string
+     */
+    public static function generate($invoiceNumber = 1, $isReminder = false)
+    {
+        $fik = str_pad($invoiceNumber, 13, 0, STR_PAD_LEFT);
 
-		$fik .= ( $isReminder ? static::REMINDER_ID : static::INVOICE_ID );
+        $fik .= ($isReminder ? static::REMINDER_ID : static::INVOICE_ID);
 
-		$fik .= static::generateCheckDigit( $fik );
+        $fik .= static::generateCheckDigit($fik);
 
-		$fik = str_pad( $fik, 15, 0, STR_PAD_LEFT );
+        $fik = str_pad($fik, 15, 0, STR_PAD_LEFT);
 
-		return $fik;
-	}
+        return $fik;
+    }
 
-	/**
-	 * @param int $invoiceNumberPlusIdentifierDigit
-	 *
-	 * @return string
-	 */
-	private static function generateCheckDigit( $invoiceNumberPlusIdentifierDigit = 10 )
-	{
-		$curMultiplier = 1;
-		$maxMultiplier = 2;
+    /**
+     * @param int $invoiceNumberPlusIdentifierDigit
+     *
+     * @return string
+     */
+    private static function generateCheckDigit($invoiceNumberPlusIdentifierDigit = 10)
+    {
+        $curMultiplier = 1;
+        $maxMultiplier = 2;
 
-		$itemArray = str_split( $invoiceNumberPlusIdentifierDigit );
+        $itemArray = str_split($invoiceNumberPlusIdentifierDigit);
 
-		for ( $i = 0, $iMax = count( $itemArray ); $i < $iMax; $i ++ ) {
-			$itemArray[ $i ] *= $curMultiplier;
+        for ($i = 0, $iMax = count($itemArray); $i < $iMax; $i++) {
+            $itemArray[$i] *= $curMultiplier;
 
-			$itemArray[ $i ] = $itemArray[ $i ] >= 10 ? ( 1 + ( $itemArray[ $i ] % 10 ) ) : $itemArray[ $i ];
+            $itemArray[$i] = $itemArray[$i] >= 10 ? (1 + ($itemArray[$i] % 10)) : $itemArray[$i];
 
-			$curMultiplier = $curMultiplier === $maxMultiplier ? 1 : $maxMultiplier;
-		}
+            $curMultiplier = $curMultiplier === $maxMultiplier ? 1 : $maxMultiplier;
+        }
 
-		$checkDigit = 0;
+        $checkDigit = 0;
 
-		array_map( function ( $item ) use ( &$checkDigit ) {
-			$checkDigit += $item;
-		}, $itemArray );
+        array_map(function ($item) use (&$checkDigit) {
+            $checkDigit += $item;
+        }, $itemArray);
 
-		$checkDigit %= 10;
-		$checkDigit = $checkDigit !== 0 ? 10 - $checkDigit : 0;
+        $checkDigit %= 10;
+        $checkDigit = $checkDigit !== 0 ? 10 - $checkDigit : 0;
 
-		return $checkDigit;
-	}
+        return $checkDigit;
+    }
 }
